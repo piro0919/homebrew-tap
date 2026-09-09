77a99e28a52b42a78cb37a03892936af4ba91d08
piro0919 <piro.haniwa@gmail.com>
2026-09-09 21:11:53 +0900

    fix: drop the deprecated verified parameter from the macview cask

--- Casks/macview.rb
+++ Casks/macview.rb
@@ -2,8 +2,7 @@ cask "macview" do
   version "0.1.0"
   sha256 "e98c9b4c095ca12d9062903eadbfb1f43557a47d752be1164685d16f881843bc"
 
-  url "https://github.com/piro0919/macview/releases/download/v#{version}/Macview-#{version}.dmg",
-      verified: "github.com/piro0919/macview/"
+  url "https://github.com/piro0919/macview/releases/download/v#{version}/Macview-#{version}.dmg"
   name "Macview"
   desc "Image viewer where the window is the image"
   homepage "https://github.com/piro0919/macview"
