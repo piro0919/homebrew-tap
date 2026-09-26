5f0feb9351cc2464e0345036ecd3f45583d7d944
piro0919 <piro.haniwa@gmail.com>
2026-09-26 09:33:21 +0900

    fix: catch gocci up to 1.1.3

--- Casks/gocci.rb
+++ Casks/gocci.rb
@@ -1,9 +1,8 @@
 cask "gocci" do
-  version "1.1.1"
-  sha256 "245d4ed04942712f28d0de70be27c317b30a98984d623b77bcf435bcf2ef4ac6"
+  version "1.1.3"
+  sha256 "193969bcd83ad52164fab36dd6d14ff2d269165bd5d6c37fe06d9bd2dcde4a4c"
 
-  url "https://github.com/piro0919/gocci/releases/download/v#{version}/Gocci-#{version}.dmg",
-      verified: "github.com/piro0919/gocci/"
+  url "https://github.com/piro0919/gocci/releases/download/v#{version}/Gocci-#{version}.dmg"
   name "Gocci"
   desc "Mounts Google Drive in Finder from the menu bar"
   homepage "https://gocci.kkweb.io"
